0365e25eee3bc5ee80b345cfe249cd9a9a05e577
Techq <tassemble@gmail.com>
2012-03-20 19:35:01 +0800

    add some test

---
+++ src/test/java/com/forest/ape/mq/impl/WorkerFactoryTest.java
@@ -0,0 +1,28 @@
+package com.forest.ape.mq.impl;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+/**
+ * create leader sender and 2 followers, when leader send msg, all the followers received.
+ * @author CHQ
+ * 2012-3-16
+ */
+public class WorkerFactoryTest {
+
+	@Test
+	public void test() throws InterruptedException {
+		WorkerFactory leaderFactory = new WorkerFactory().buildByLeader("2", Arrays.asList("1", "3"), "521");
+		leaderFactory.start();
+		
+		WorkerFactory followerFactory = new WorkerFactory().buildByFollower("3", "521", new DefaultRecvCallableHandler());
+		followerFactory.start();
+		
+		
+		leaderFactory.enQueue(new MQPacket("hahah".getBytes(), null));
+		Thread.currentThread().join();
+	}
+	
+	
+}
